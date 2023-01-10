6eb917f16796d033d9f0b3333104efc756b83e8a
Charles Michael Dickens <cmdickens@login2.cluster>
2023-01-10 15:20:45 -0600

    adding interproscan

--- templates/grace/run_interproscan_5.60-92.0_protein_grace.sh
+++ templates/grace/run_interproscan_5.60-92.0_protein_grace.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --export=NONE               # do not export current env to the job
-#SBATCH --job-name=ips-5.53-87.0    # job name
+#SBATCH --job-name=ips-5.60-92.0    # job name
 #SBATCH --time=1-00:00:00           # max job run time dd-hh:mm:ss
 #SBATCH --ntasks-per-node=1         # tasks (commands) per compute node
-#SBATCH --cpus-per-task=12          # CPUs (threads) per command
-#SBATCH --mem=90G                   # total memory per node
+#SBATCH --cpus-per-task=8           # CPUs (threads) per command
+#SBATCH --mem=60G                   # total memory per node
 #SBATCH --output=stdout.%x.%j       # save stdout to file
 #SBATCH --error=stderr.%x.%j        # save stderr to file
 
-module load GCC/9.3.0  OpenMPI/4.0.3  InterProScan/5.53-87.0-Python-3.8.2
+module load  GCC/11.2.0  OpenMPI/4.1.1 InterProScan/5.60-92.0
 module load WebProxy
 
 <<README
@@ -17,12 +17,13 @@ README
 
 ################################### VARIABLES ##################################
 # TODO Edit these variables as needed:
+
 ########## INPUTS ##########
 protein_fasta_file='/scratch/data/bio/GCATemplates/data/ips/erg_prot.fa'
 
 ######## PARAMETERS ########
-# select one or more of the following; notice some are for bacteria only: SignalP. The ProDom application is deprecated
-applications='TIGRFAM,PIRSF,SMART,SFLD,PrositeProfiles,PrositePatterns,HAMAP,Phobius,PfamA,PRINTS,SuperFamily,Coils,SignalP-GRAM_POSITIVE,SignalP-GRAM_NEGATIVE,SignalP-EUK,TMHMM,PANTHER'
+# select one or more of the following; notice some are for bacteria only: SignalP.
+applications='TIGRFAM,FunFam,SFLD,Phobius,SignalP_GRAM_NEGATIVE,PANTHER,Gene3D,Hamap,PRINTS,ProSiteProfiles,Coils,SUPERFAMILY,SMART,CDD,PIRSR,ProSitePatterns,AntiFam,SignalP_EUK,Pfam,MobiDBLite,SignalP_GRAM_POSITIVE,PIRSF,TMHMM'
 
 # supported formats are JSON, TSV, XML and GFF3. The HTML and SVG formats are deprecated
 formats='JSON,TSV,XML,GFF3'
@@ -34,7 +35,7 @@ out_dir="out_ips_$EBVERSIONINTERPROSCAN"
 [ -d $out_dir ] || mkdir $out_dir
 
 interproscan.sh --tempdir $TMPDIR --cpu $SLURM_CPUS_PER_TASK --input $protein_fasta_file \
-  --applications $applications  --formats $formats  --output-dir $out_dir
+  --applications $applications  --formats $formats  --output-dir $out_dir --disable-precalc
 
 ################################################################################
 <<CITATION
